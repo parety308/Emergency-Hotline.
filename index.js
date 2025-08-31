1bac58437ce4affc60941fce11e4488af919272a
parvez <parvezyesrat17032024@gmail.com>
2025-09-01 03:24:07 +0600

    added functional commit

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const copyNumbers = [];
 let copyCount = 0;
 let coinCount = 100;
 let heartCount = 0;
-function coin_Count() {
-    if (coinCount < 20) {
-        alert('You do not have enough balance. You need at least 20 Coin to make a call.');
-        return;
-    }
+function copy_Count() {
+    copyCount++;
+    let cpy = document.getElementById('copy-count');
+    cpy.innerText = copyCount;
+
+}
+function addStore() {
+    const store = document.getElementById('store');
     coinCount -= 20;
     const totalCoin = document.getElementById('coin-count');
     totalCoin.innerText = coinCount;
-}
-function addStore(count) {
-    const store = document.getElementById('store');
-    let cpy = document.getElementById('copy-count');
-    cpy.innerText = count;
     store.innerHTML = "";
 
     for (const num of copyNumbers) {
@@ -34,7 +32,6 @@ function addStore(count) {
 }
 
 function getNum(title, num) {
-    copyCount++;
     const numb = document.getElementById(num).innerText;
     const nam = document.getElementById(title).innerText;
     const data = {
@@ -43,63 +40,62 @@ function getNum(title, num) {
         time: new Date().toLocaleTimeString()
     }
     copyNumbers.push(data);
-    addStore(copyCount);
-
+    addStore();
 }
 
 document.getElementById('national-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('national-title', 'national-num');
     });
 
 document.getElementById('police-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('police-title', 'police-num');
     });
 
 document.getElementById('fire-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('fire-title', 'fire-num');
     });
 
 document.getElementById('ambulance-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('ambulance-title', 'ambulance-num');
     });
 
 document.getElementById('child-women-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('child-women-title', 'child-women-num');
     });
 
 document.getElementById('anti-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('anti-title', 'anti-num');
     });
 
 document.getElementById('electricity-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('electricity-title', 'electricity-num');
     });
 
 document.getElementById('brac-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('brac-title', 'brac-num');
     });
 
 
 document.getElementById('railway-copy')
     .addEventListener('click', function () {
+        copy_Count()
 
-        getNum('railway-title', 'railway-num');
     });
 document.getElementById('clear-btn')
     .addEventListener('click', function () {
@@ -112,59 +108,94 @@ document.getElementById('clear-btn')
 //Call Functionality
 document.getElementById('national-call')
     .addEventListener('click', function () {
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
         alert('Calling National Emergency Number 999.. ');
-
-    coin_Count();
+        getNum('national-title', 'national-num');
     });
 
 document.getElementById('police-call')
     .addEventListener('click', function () {
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
         alert('Calling Police Helpline Number 999...');
-    coin_Count();
+        getNum('police-title', 'police-num');
     });
 
 document.getElementById('fire-call')
     .addEventListener('click', function () {
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
         alert('Calling Fire Service Number 999...');
-    coin_Count();
+        getNum('fire-title', 'fire-num');
     });
 
 document.getElementById('ambulance-call')
     .addEventListener('click', function () {
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
         alert('Ambulance Service 1994-999999..');
-    coin_Count();
+        getNum('ambulance-title', 'ambulance-num');
     });
 
 document.getElementById('child-women-call')
     .addEventListener('click', function () {
-alert('Calling Women & Child Helpline 109...')
-coin_Count();    
-});
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
+        alert('Calling Women & Child Helpline 109...')
+        getNum('child-women-title', 'child-women-num');
+    });
 
 document.getElementById('anti-call')
     .addEventListener('click', function () {
-alert('Calling Anti-Corruption Helpline 106...')
-coin_Count();    
-});
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
+        alert('Calling Anti-Corruption Helpline 106...')
+        getNum('anti-title', 'anti-num');
+    });
 
 document.getElementById('electricity-call')
     .addEventListener('click', function () {
-alert('Calling Electricity Helpline 16216....')
-coin_Count();    
-});
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
+        alert('Calling Electricity Helpline 16216....')
+        getNum('electricity-title', 'electricity-num');
+    });
 
 document.getElementById('brac-call')
     .addEventListener('click', function () {
-alert('Calling Brac Helpline 16445...')
-coin_Count();    
-});
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
+        alert('Calling Brac Helpline 16445...')
+        getNum('brac-title', 'brac-num');
+    });
 
 
 document.getElementById('railway-call')
     .addEventListener('click', function () {
-alert('Calling Bangladesh Railway Helpline 163.. ');
-coin_Count();    
-});
+        if (coinCount < 20) {
+            alert('You do not have enough balance. You need at least 20 Coin to make a call.');
+            return;
+        }
+        alert('Calling Bangladesh Railway Helpline 163.. ');
+        getNum('railway-title', 'railway-num');
+    });
 
 const hearts = document.getElementsByClassName('heart-count');
 
